c69a588bd982ef0d8ba6362030242a33fd3a65a7
Liu-2001 <1034368684@qq.com>
2020-10-09 17:58:28 +0800

    update exerciselist class

--- src/main/java/seedu/address/model/exercise/ExerciseList.java
+++ src/main/java/seedu/address/model/exercise/ExerciseList.java
@@ -1,4 +1,48 @@
 package seedu.address.model.exercise;
 
-public class ExerciseList {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.Iterator;
+
+import static java.util.Objects.requireNonNull;
+
+public class ExerciseList implements Iterable<Exercise> {
+
+    private final ObservableList<Exercise> internalList = FXCollections.observableArrayList();
+    private final ObservableList<Exercise> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Adds an exercise to the list.
+     */
+    public void add(Exercise toAdd) {
+        requireNonNull(toAdd);
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Exercise> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<Exercise> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ExerciseList // instanceof handles nulls
+                && internalList.equals(((ExerciseList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
 }
